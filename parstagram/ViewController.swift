53b9e3b14fe0355c48d1b481a5d9ed86fcd794f6
orcunTasdemir <otasdemir@vassar.edu>
2022-01-07 07:38:25 -0500

    Initial Commit

---
+++ parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parstagram
+//
+//  Created by OrcunTasdemir on 1/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
